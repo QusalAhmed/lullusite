786161df94548ce438309b82abd297a7f2ede3ac
Qusal <qusal15@student.sust.edu>
2025-11-21 10:16:47 +0600

    Fetch products

--- src/actions/product/get-products.ts
+++ src/actions/product/get-products.ts
@@ -1,49 +1,49 @@
-// 'use server';
-//
-// // db
-// import db from "@/lib/drizzle-agent";
-// import { eq } from "drizzle-orm";
-// import { productTable } from "@/db/product.schema";
-//
-// // Auth
-// import getSession from "@/lib/get-session";
-//
-// export default async function getProducts(limit: number = 10) {
-//     const session = await getSession();
-//
-//     return db
-//         .query
-//         .productTable
-//         .findMany({
-//             columns: {
-//                 id: true,
-//                 name: true,
-//                 isActive: true,
-//             },
-//             with: {
-//                 images: {
-//                     columns: {
-//                         image: true,
-//                     }
-//                 },
-//                 variations: {
-//                     columns: {
-//                         id: true,
-//                         name: true,
-//                         sku: true,
-//                         price: true,
-//                     },
-//                     with: {
-//                         image: {
-//                             columns: {
-//                                 productVariationId: true,
-//                                 image: true,
-//                             }
-//                         },
-//                     },
-//                 }
-//             },
-//             where: eq(productTable.merchantId, session.user.id),
-//             limit: limit,
-//         })
-// }
+'use server';
+
+// db
+import db from "@/lib/drizzle-agent";
+import { eq } from "drizzle-orm";
+import { productTable } from "@/db/product.schema";
+
+// Auth
+import getSession from "@/lib/get-session";
+
+export default async function getProducts(limit: number = 10) {
+    const session = await getSession();
+
+    return db
+        .query
+        .productTable
+        .findMany({
+            columns: {
+                id: true,
+                name: true,
+                isActive: true,
+            },
+            with: {
+                images: {
+                    columns: {
+                        image: true,
+                    }
+                },
+                variations: {
+                    columns: {
+                        id: true,
+                        name: true,
+                        sku: true,
+                        price: true,
+                    },
+                    with: {
+                        image: {
+                            columns: {
+                                productVariationId: true,
+                                image: true,
+                            }
+                        },
+                    },
+                }
+            },
+            where: eq(productTable.merchantId, session.user.id),
+            limit: limit,
+        })
+}
