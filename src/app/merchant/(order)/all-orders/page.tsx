a5b40a81ec31e1f42a91ecbaab2a28b432ba1791
Qusal <qusal15@student.sust.edu>
2026-01-17 15:07:04 +0600

    Order view url sync with tabs

--- src/app/merchant/(order)/all-orders/page.tsx
+++ src/app/merchant/(order)/all-orders/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React from 'react';
+import {useRouter, useSearchParams} from "next/navigation";
 
 // Local
 import OrderTable from './order-table';
@@ -13,8 +14,10 @@ import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area"
 import ORDER_STATUS from '@/constant/order-status';
 
 const Page = () => {
-    const [activeTab, setActiveTab] = React.useState("all-orders");
     const tabs = [{ value: "all-orders", label: "All Orders" }, ...ORDER_STATUS];
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const [activeTab, setActiveTab] = React.useState(searchParams.get('status') || 'all-orders');
 
     return (
         <>
@@ -26,7 +29,10 @@ const Page = () => {
                             variant="outline"
                             size="sm"
                             className={activeTab === tab.value ? 'bg-secondary' : ''}
-                            onClick={() => setActiveTab(tab.value)}
+                            onClick={() => {
+                                router.push(`/merchant/all-orders${tab.value === 'all-orders' ? '' : `?status=${tab.value}`}`)
+                                setActiveTab(tab.value)
+                            }}
                         >
                             {tab.label}
                         </Button>
@@ -37,57 +43,6 @@ const Page = () => {
             <OrderTable status={activeTab === 'all-orders' ? undefined : activeTab}/>
         </>
     )
-
-    // return (
-    //     <>
-    //         <section className="mb-4">
-    //             <Tabs defaultValue="all-orders">
-    //                 <TabsList className="overflow-x-auto">
-    //                     <TabsTrigger value="all-orders">All Orders</TabsTrigger>
-    //                     <TabsTrigger value="pending-orders">Pending Orders</TabsTrigger>
-    //                     <TabsTrigger value="confirmed">Confirmed Order</TabsTrigger>
-    //                     <TabsTrigger value="toship">To Ship</TabsTrigger>
-    //                     <TabsTrigger value="shipping">Shipping</TabsTrigger>
-    //                     <TabsTrigger value="delivered">Delivered</TabsTrigger>
-    //                     <TabsTrigger value="partially-delivered">Partially Delivered</TabsTrigger>
-    //                     <TabsTrigger value="returned">Returned</TabsTrigger>
-    //                     <TabsTrigger value="cancelled">Cancellation</TabsTrigger>
-    //                     <TabsTrigger value="refund">Refunded</TabsTrigger>
-    //                 </TabsList>
-    //                 <TabsContent value="all-orders">
-    //                     <OrderTable/>
-    //                 </TabsContent>
-    //                 <TabsContent value="pending-orders">
-    //                     <OrderTable status="pending"/>
-    //                 </TabsContent>
-    //                 <TabsContent value="confirmed">
-    //                     <OrderTable status="confirmed"/>
-    //                 </TabsContent>
-    //                 <TabsContent value="toship">
-    //                     <OrderTable status="ready_to_ship"/>
-    //                 </TabsContent>
-    //                 <TabsContent value="shipping">
-    //                     <OrderTable status="shipped"/>
-    //                 </TabsContent>
-    //                 <TabsContent value="delivered">
-    //                     <OrderTable status="delivered"/>
-    //                 </TabsContent>
-    //                 <TabsContent value="partially-delivered">
-    //                     <OrderTable status="partially_delivered"/>
-    //                 </TabsContent>
-    //                 <TabsContent value="returned">
-    //                     <OrderTable status="returned"/>
-    //                 </TabsContent>
-    //                 <TabsContent value="cancelled">
-    //                     <OrderTable status="cancelled"/>
-    //                 </TabsContent>
-    //                 <TabsContent value="refund">
-    //                     <OrderTable status="refunded"/>
-    //                 </TabsContent>
-    //             </Tabs>
-    //         </section>
-    //     </>
-    // );
 };
 
 export default Page;
