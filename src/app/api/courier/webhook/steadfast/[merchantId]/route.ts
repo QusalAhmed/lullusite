f93f66a251e24c3dba2bde8df0f2b8aa16b169df
Qusal <qusal15@student.sust.edu>
2026-01-20 16:21:02 +0600

    Steadfast webhook started

--- src/app/api/courier/webhook/steadfast/[merchantId]/route.ts
+++ src/app/api/courier/webhook/steadfast/[merchantId]/route.ts
@@ -1,21 +1,3 @@
-import { NextResponse, NextRequest } from 'next/server';
-
-export async function POST(
-    request: NextRequest,
-    {params}: { params: { merchantId: string } }
-) {
-    const {merchantId} = params
-    const requestBody = await request.json();
-    console.log(`Received webhook for merchant ID: ${merchantId}`);
-    console.log('Request body:', requestBody);
-
-    try {
-        const text = await request.text()
-        console.log('Raw request body:', text)
-        // Process the webhook payload
-    } catch (error) {
-        return NextResponse.json({status: 'error', message: error}, {status: 500});
-    }
-
-    return NextResponse.json({status: 'success', message: 'Webhook received successfully.'});
+export async function GET() {
+    return Response.json({ message: 'Hello World' })
 }
