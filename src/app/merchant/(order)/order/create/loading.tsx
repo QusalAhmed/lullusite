6d91da1c02b7d875e73324f8a68db9fe4097d46a
Qusal <qusal15@student.sust.edu>
2026-01-26 14:57:42 +0600

    Create order loading.tsx

---
+++ src/app/merchant/(order)/order/create/loading.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// ShadCN
+import { Spinner } from "@/components/ui/spinner"
+
+const Loading = () => {
+    return (
+        <div className="flex items-center justify-center gap-4 py-10">
+            <Spinner className="size-12" />
+            <div className={'text-lg'}>Loading form...</div>
+        </div>
+    );
+};
+
+export default Loading;
